22e3b7ff88366c9226d7001e0848405d6032657b
Kalpesh Prithyani <kalpeshprithyani@Kalpeshs-MacBook-Pro.local>
2019-02-28 00:01:31 +0530

    Updates

---
+++ precache-manifest.de2cc072d94422c48bfd632a9ec8b4cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77aa169a366d39c470be",
+    "url": "/React-based-todo/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "77aa169a366d39c470be",
+    "url": "/React-based-todo/static/js/main.77aa169a.chunk.js"
+  },
+  {
+    "revision": "97bccb7d85772525810c",
+    "url": "/React-based-todo/static/js/runtime~main.97bccb7d.js"
+  },
+  {
+    "revision": "82003dedbbb4b1efef05",
+    "url": "/React-based-todo/static/js/2.82003ded.chunk.js"
+  },
+  {
+    "revision": "28314a497d4f687c0758096e9923ae32",
+    "url": "/React-based-todo/index.html"
+  }
+];
